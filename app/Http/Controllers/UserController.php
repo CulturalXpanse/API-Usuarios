02188380333e280d99d212040e6f66739db7c3bd
Kevinpereira13 <kevinbrian2013@gmail.com>
2024-10-25 13:21:20 -0300

    Creado el UserController + 4 funciones(Register, createUser, ValidateToken y Logout)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\Models\User;
+
 class UserController extends Controller
 {
+    public function Register(Request $request){
 
+        $validation = Validator::make($request->all(),[
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        if($validation->fails())
+            return $validation->errors();
+
+        return $this -> createUser($request);
+
+    }
+
+    private function createUser($request){
+        $user = new User();
+        $user -> name = $request -> post("name");
+        $user -> email = $request -> post("email");
+        $user -> password = Hash::make($request -> post("password"));
+        $user -> save();
+        return $user;
+    }
+
+    public function ValidateToken(Request $request){
+        return auth('api')->user();
+    }
+
+    public function Logout(Request $request){
+        $request->user()->token()->revoke();
+        return ['message' => 'Token Revoked'];
+    }
 }
