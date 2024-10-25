c4447a3ed2f1ae084745a16e15c20081843e0f29
Kevinpereira13 <kevinbrian2013@gmail.com>
2024-10-25 13:08:39 -0300

    Primer commit subiendo el archivo laravel api-usuarios

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Support\Str;
+
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+}
